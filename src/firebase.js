4cb1d6806052c7c69114e7f16481d5c476d01d9d
Rustam <rustam905@gmail.com>
2021-02-26 16:50:06 -0800

    App created

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDKA8AhQczVMoIhrDZSTspnyz9dN6OVaVo",
+	authDomain: "slack-clone-d2681.firebaseapp.com",
+	projectId: "slack-clone-d2681",
+	storageBucket: "slack-clone-d2681.appspot.com",
+	messagingSenderId: "218575846812",
+	appId: "1:218575846812:web:bc3874ab3c5349140e1c59",
+};
